acdb897c17d9c7dd81b1a82608b5b0a1e0b8bcb5
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-15 15:08:44 +1100

    ch3: Screen clearing implemented using VT100 based ANSI escape sequences

--- kilo.c
+++ kilo.c
@@ -65,10 +65,16 @@ void editor_process_keypress() {
     }
 }
 
+void editor_refresh_screen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 int main() {
     enable_raw_mode();
 
     while (1) {
+        editor_refresh_screen();
         editor_process_keypress();
     }
 
