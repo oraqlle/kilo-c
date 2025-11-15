73ddf31cb57582419f078db693131b0b72e2ae24
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-15 14:08:48 +1100

    ch2: Canonical mode disable when kilo starts

--- kilo.c
+++ kilo.c
@@ -12,7 +12,7 @@ void enable_raw_mode() {
     atexit(disable_raw_mode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
