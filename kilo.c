bd655dfc82eb8cec38439fca287dd47007a2e7ae
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-15 15:13:54 +1100

    ch3: kilo draws tildes on empty rows and now has global state store

--- kilo.c
+++ kilo.c
@@ -1,34 +1,40 @@
 #include <termios.h>
 #include <unistd.h>
 
-#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define CTRL_KEY(key) (key) & 0x1f
 
-static struct termios orig_termios;
+typedef struct {
+    struct termios orig_termios;
+} editor_config;
+
+static editor_config e_config;
 
 void die(const char *str) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(str);
     exit(1);
 }
 
 void disable_raw_mode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &e_config.orig_termios) == -1) {
         die("disable_raw_mode :: tcsetattr");
     }
 }
 
 void enable_raw_mode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &e_config.orig_termios) == -1) {
         die("enable_raw_mode :: tcgetattr");
     }
 
     atexit(disable_raw_mode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = e_config.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -60,13 +66,25 @@ void editor_process_keypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
 }
 
+void editor_draw_rows() {
+    for (unsigned y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editor_refresh_screen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editor_draw_rows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
