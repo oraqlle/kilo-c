201e1a44c2a9cbe6c245eecd99ee93d042d4e4b5
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-15 14:05:16 +1100

    ch2: termios state restored on exit of kilo

--- kilo.c
+++ kilo.c
@@ -1,11 +1,19 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include <stdlib.h>
+
+static struct termios orig_termios;
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enable_raw_mode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode);
 
-    tcgetattr(STDIN_FILENO, &raw);
-
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
